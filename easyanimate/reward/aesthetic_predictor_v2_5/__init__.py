9b89cdacebb5193a0b71a624d5d16952e18ef307
hkunzhe <huangkunzhe.hkz@alibaba-inc.com>
2024-11-27 15:13:46 +0800

    add reward lora

---
+++ easyanimate/reward/aesthetic_predictor_v2_5/__init__.py
@@ -0,0 +1,13 @@
+from .siglip_v2_5 import (
+    AestheticPredictorV2_5Head,
+    AestheticPredictorV2_5Model,
+    AestheticPredictorV2_5Processor,
+    convert_v2_5_from_siglip,
+)
+
+__all__ = [
+    "AestheticPredictorV2_5Head",
+    "AestheticPredictorV2_5Model",
+    "AestheticPredictorV2_5Processor",
+    "convert_v2_5_from_siglip",
+]
